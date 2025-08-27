4d567dea03eb52c80b4d6ade1024b95d14bdef43
Computerman80186 <computermangithub@OUTLOOK.COM>
2025-08-27 13:37:47 +0200

    Update main_server.py

--- main_server.py
+++ main_server.py
@@ -6,6 +6,12 @@ def main():
   global s
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.bind('localhost', 12345)
-  s.listen(1)
-class client:
+  while Listening:
+    s.listen(1)
+    
+  
+class client(supplied_conn, supplied_addr):
+  def __init__(self, conn, addr):
+    self.conn = supplied_conn
+    self.addr = supplied_addr
   
